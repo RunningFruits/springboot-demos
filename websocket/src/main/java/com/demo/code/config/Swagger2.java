3d431a99b889cb890a3c37715e4a0e7fc022ac65
brightereyer2 <brightereyer2@163.com>
2020-03-26 23:14:42 +0800

    + log

---
+++ websocket/src/main/java/com/demo/code/config/Swagger2.java
@@ -0,0 +1,39 @@
+package com.demo.code.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.demo.code.controller"))//单路径扫描
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("项目接口文档")//项目描述1
+                .description("简单优雅的restful风格")//项目描述2
+                .termsOfServiceUrl("http://")//项目描述3
+                .version("1.0")
+                .build();
+    }
+
+}
+
