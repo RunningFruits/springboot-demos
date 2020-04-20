b2f4809d208fd91456740278102f2cd80b0b7bf8
brightereyer2 <brightereyer2@163.com>
2020-04-20 09:43:10 +0800

    *

--- email/src/main/java/com/lzp/EmailBoot.java
+++ email/src/main/java/com/lzp/EmailBoot.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootEmailApplication {
+public class EmailBoot {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEmailApplication.class, args);
+		SpringApplication.run(EmailBoot.class, args);
 	}
 }
