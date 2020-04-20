ca64c48694a855b4cc43eb9c966aa197f81fd943
brightereyer2 <brightereyer2@163.com>
2020-04-20 16:56:34 +0800

    *

--- qiniu-layui/src/main/java/com/code/util/QiniuUtil.java
+++ qiniu-layui/src/main/java/com/code/util/QiniuUtil.java
@@ -15,24 +15,28 @@ public class QiniuUtil {
     private static String bucket;
 
     @Value("${qiniu.accesskey}")
-    private  void setAk(String ak) {
+    private void setAk(String ak) {
         QiniuUtil.ak = ak;
     }
+
     @Value("${qiniu.secretkey}")
-    private  void setSk(String sk) {
+    private void setSk(String sk) {
         QiniuUtil.sk = sk;
     }
+
     @Value("${qiniu.bucket}")
-    private  void setBucket(String bucket) {
+    private void setBucket(String bucket) {
         QiniuUtil.bucket = bucket;
     }
-    private static String token(){
+
+    private static String token() {
         Auth auth = Auth.create(ak, sk);
         return auth.uploadToken(bucket);
     }
-    public static void upload(String path,String name) throws QiniuException {
+
+    public static void upload(String path, String name) throws QiniuException {
         Configuration configuration = new Configuration(Zone.zone2());
         UploadManager uploadManager = new UploadManager(configuration);
-        uploadManager.put(path,name,token());
+        uploadManager.put(path, name, token());
     }
 }
