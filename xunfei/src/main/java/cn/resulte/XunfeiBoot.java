318869946a3e8028f410c71159795b73b4a3833f
brightereyer2 <brightereyer2@163.com>
2020-05-03 15:18:37 +0800

    *

---
+++ xunfei/src/main/java/cn/resulte/XunfeiBoot.java
@@ -0,0 +1,11 @@
+package cn.resulte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunfeiBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(XunfeiBoot.class,args);
+    }
+}
---
+++ xunfei/src/main/java/cn/resulte/controller/IndexController.java
@@ -0,0 +1,54 @@
+package cn.resulte.controller;
+
+import cn.resulte.xunfei.XunfeiToVoice;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
+@Slf4j
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @PostMapping("/xunfeiConvert")
+    public String xunfeiConvert(HttpServletRequest request, HttpServletResponse response) {
+
+        String message = request.getParameter("message");
+        String index = request.getParameter("index");
+        log.info(index);
+
+        String path = null;
+        try {
+            path = XunfeiToVoice.changeToVoice(message, Integer.parseInt(index));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info(path);
+        response.setHeader("Content-type", "text/html;charset=UTF-8");
+        /*
+        try {
+            PrintWriter out = response.getWriter();
+            out.println(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        return path;
+
+    }
+
+}
